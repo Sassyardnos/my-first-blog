c25614a3741fa6e7bd9e2413d5bdeb2e5ca86a80
sassyardnos <sondra.locke@hotmail.com>
2023-08-24 15:53:22 +1000

    Add new post, edit post and authenticated user

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Post
+
 class PostForm(forms.ModelForm):
+
     class Meta:
-        model = Postfield = ('title', 'text',)
+        model = Post
+        fields = ('title', 'text',)
